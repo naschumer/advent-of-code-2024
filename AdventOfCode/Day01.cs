d34c2693ab79a328ac93fa909aa0f4d4dc013f9f
Nick Schumer <42980869+naschumer@users.noreply.github.com>
2024-12-01 17:55:19 -0500

    day1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,38 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        GetLists(_input, out var list1, out var list2);
+        list1 = list1.OrderBy(x => x).ToList();
+        list2 = list2.OrderBy(x => x).ToList();
+        list1 = list1.Select((x, i) => Math.Abs(x - list2[i])).ToList();
+        return new(list1.Sum().ToString());
+    }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public void GetLists(string[] input, out List<int> list1, out List<int> list2)
+    {
+        list1 = new List<int>();
+        list2 = new List<int>();
+        foreach (var line in _input)
+        {
+            var c = line.Split("   ");
+            list1.Add(int.Parse(c[0]));
+            list2.Add(int.Parse(c[1]));
+        }
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        GetLists(_input, out var list1, out var list2);
+        list1 = list1.Select((x, i) => x * list2.Where(y=> y == x).Count()).ToList();
+        return new(list1.Sum().ToString());
+    }
 }
